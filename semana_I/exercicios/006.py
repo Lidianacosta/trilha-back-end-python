a14c4b2cf291ac891bdd8643625bcec3541d6697
Lidiana costa <116831875+Lidianacosta@users.noreply.github.com>
2023-09-29 14:22:21 +0000

    desafio 006 completo

---
+++ semana_I/exercicios/006.py
@@ -0,0 +1,9 @@
+print('hello world!')
+
+# crie um algoritmo que leia um numero e mostre o seu dobro,
+# triplo e raiz quadrada
+
+n1 = int(input('digite um numero: '))
+
+print('dobro = {}\ntriplo = {}\nraiz quadrada = {}'
+      .format(n1*2, n1*3, n1**(1/2)))
